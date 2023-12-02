eec071d00e6e81366f17dc5c32c24ddd961da308
anastasiak36 <akelnik.9@gmail.com>
2023-12-01 21:25:16 -0500

    wrote fetch commands to display data on logged in page (has errors)

--- app/api/products/page.jsx
+++ app/api/products/page.jsx
@@ -2,6 +2,18 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
+// export default async function handler(req, res) {
+//     try {
+//       const products = await prisma.products.findMany(); // Assuming your model is named 'Product'
+//       res.json(products);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     } finally {
+//       await prisma.$disconnect();
+//     }
+//   }
+
 export default async function main() {
     try {
         const allProducts = await prisma.products.findMany();
--- app/logged-in/page.jsx
+++ app/logged-in/page.jsx
@@ -1,10 +1,41 @@
+ 'use client'
+
 import Link from 'next/link'
+import { useState, useEffect } from 'react';
 
 
-export default function Loggedin() {
+ export default function Loggedin() {
+//     const { data, error } = useSWR('/api/products', fetcher)
+//   if (error) return <div>An error occured.</div>
+//   if (!data) return <div>Loading ...</div>
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('/api/products'); 
+            const data = await response.json();
+            console.log('Fetched Data:', data); 
+            setProducts(data);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+    
+        fetchData();
+      }, []);
+
     return (
         <main>
             <h2>Click the button!</h2>
+            <h1>User Data</h1>
+            <ul>
+            {products.map((product) => (
+                <li key={product.product_id}>
+                {product.product_name} 
+                </li>
+            ))}
+            </ul>
             <Link href="/profile">Go To Profile</Link>
         </main>
     )

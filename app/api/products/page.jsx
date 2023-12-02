aefda6a71697fe532f42fd6ce693a9c9b11f7145
anastasiak36 <akelnik.9@gmail.com>
2023-12-01 23:12:08 -0500

    changed prisma fetching to use swr instead of just fetch

--- app/api/products/page.jsx
+++ app/api/products/page.jsx
@@ -2,31 +2,22 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-// export default async function handler(req, res) {
-//     try {
-//       const products = await prisma.products.findMany(); // Assuming your model is named 'Product'
-//       res.json(products);
-//     } catch (error) {
-//       console.error(error);
-//       res.json({ error: 'Internal Server Error' });
-//     } finally {
-//       await prisma.$disconnect();
-//     }
-//   }
-
-export default async function main() {
-    try {
-        const allProducts = await prisma.products.findMany();
-    console.log('fetched data frojm products', allProducts);
-    }
-    catch (error){
-        console.error(error)
-        await prisma.$disconnect()
-        process.exit(1)
-    }
-    finally {
-        await prisma.$disconnect()
-    }
-    
+export default async function handler(req, res) {
+      const products = await prisma.products.findMany(); // Assuming your model is named 'Product'
+      res.json(products);
 }
+
+// export default async function main() {
+//     try {
+//         const allProducts = await prisma.products.findUnique({
+//             where: { product_id: 1},
+//         });
+//     console.log('fetched data frojm products', allProducts);
+//     }
+//     catch (error){
+//         console.error(error)
+//         process.exit(1)
+//     }
+    
+// }
 
--- app/logged-in/page.jsx
+++ app/logged-in/page.jsx
@@ -1,42 +1,44 @@
  'use client'
 
 import Link from 'next/link'
-import { useState } from 'react';
+import useSWR from 'swr'
+//import { useState } from 'react';
 
-
+const fetcher = url => fetch(url).then(res => res.json())
  export default function Loggedin() {
-//     const { data, error } = useSWR('/api/products', fetcher)
-//   if (error) return <div>An error occured.</div>
-//   if (!data) return <div>Loading ...</div>
-    const [products, setProducts] = useState([]);
+    const { data, error } = useSWR('/api/products', fetcher)
+  if (error) return <div>error {error}</div>
+  if (!data) return <div>Loading ...</div>
+  return (
+    // <main>
+    //     <h2>Click the button!</h2>
+    //     <h1>User Data</h1>
+    <ul>
+        {data.products.map((product) => (
+            <li key={product.product_id}>
+            {product.product_name} 
+            </li>
+        ))}
+    </ul> 
+    //      <Link href="/profile">Go To Profile</Link>
+    //  </main>
+)
+//     const [products, setProducts] = useState([]);
 
-   // useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch('/api/products', {cache : 'no-store'}); 
-            const data = await response.json();
-            console.log('Fetched Data:', data); 
-            setProducts(data);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
+//    // useEffect(() => {
+//         const fetchData = async () => {
+//           try {
+//             const response = await fetch('/api/products', {cache : 'no-store'}); 
+//             const data = await response.json();
+//             console.log('Fetched Data:', data); 
+//             setProducts(data);
+//           } catch (error) {
+//             console.error('Error fetching data:', error);
+//           }
+//         };
     
-        fetchData();
-     // }, []);
+//         fetchData();
+//      // }, []);
 
-    return (
-        <main>
-            <h2>Click the button!</h2>
-            <h1>User Data</h1>
-            <ul>
-            {products.map((product) => (
-                <li key={product.product_id}>
-                {product.product_name} 
-                </li>
-            ))}
-            </ul>
-            <Link href="/profile">Go To Profile</Link>
-        </main>
-    )
+   
 }

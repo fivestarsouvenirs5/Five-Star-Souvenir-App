b3ef2f54b57b8d82173d859e39007341892553ab
anastasiak36 <akelnik.9@gmail.com>
2023-12-11 13:24:54 -0500

    fetched all categories in order page

---
+++ app/components/category.jsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+
+// need to style
+const Category = ({ category }) => {
+
+  return (
+    <Link
+      href={`/order/${category.category}`}
+    >
+      <h2>{category.category}</h2>
+    </Link>
+  )
+}
+
+export default Category
--- app/order/page.jsx
+++ app/order/page.jsx
@@ -1,8 +1,34 @@
-export default async function Order() {
+// import 'materialize-css/dist/css/materialize.min.css';
+// import 'materialize-css'
+import Link from 'next/link'
+import prisma from '../utils/prisma'
+import Category from '../components/category'
+//import AddProductForm from '../components/addProductForm'
 
+const fetchCategories = async () => {
+    let categories = await prisma.category.findMany()
+    return categories
+}
+
+//import useSWR from 'swr'
+//import { useState } from 'react';
+
+// const fetcher = url => fetch(url).then(res => res.json())
+export default async function Order() {
+    const categories = await fetchCategories()
+
+    console.log({categories})
+//     const { data, error } = useSWR('/api/products', fetcher)
+//   if (error) return <div>error {error}</div>
+//   if (!data) return <div>Loading ...</div>
   return (
     <main>
-        <div></div>
+        <h2 class="text-red-600">Categories</h2>
+            {categories.map((category) => (
+                <div key={category.category_id} >
+                    <Category category = {category} />
+                </div>
+            ))}
     </main>
 )
 }

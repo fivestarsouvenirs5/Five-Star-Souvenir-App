b5833c3b03439b228b2cead0cd45a97c1ff1e3a0
Christina Tashji <christinast2020@gmail.com>
2023-12-11 11:37:34 -0500

    added nav bar! (ugly)

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -8,6 +8,27 @@ export default function NavBar() {
         <a href="/api/auth/login">Login</a>
         <a href="/api/auth/logout">Logout</a>
         
+        {/* <img className="h-auto max-w-full" src="/app/imgs/Logo.png" alt="image"> */}
+        
+        {/* Nav bar */}
+        <div className="w-[1195px] h-[49px] relative">
+        <div className="w-[1195px] h-[49px] left-0 top-0 absolute">
+            <div className="w-[1195px] h-[49px] left-0 top-0 absolute bg-red-400" />
+            <div className="w-[100px] h-9 left-[74px] top-[6px] absolute text-black text-[32px] font-normal font-['Inter']">
+                <a href="localhost:3000">home</a>
+            </div>
+        </div>
+        <div className="w-[139px] h-9 left-[370px] top-[6px] absolute text-black text-[32px] font-normal font-['Inter']">
+            <a href="/products/page.jsx">products</a>
+        </div>
+        <div className="w-[139px] h-9 left-[686px] top-[6px] absolute text-black text-[32px] font-normal font-['Inter']">
+            <a href="/contact/page.jsx">contact</a>
+        </div>
+        <div className="w-[139px] h-9 left-[1002px] top-[6px] absolute text-black text-[32px] font-normal font-['Inter']">
+            <a href="/about-us/page.jsx">about us</a>
+            <br/></div>
+        </div>
+        
     </nav>
     )
    

42c7bba536d55dd351bd919136a398d4832a38f5
Christina Tashji <christinast2020@gmail.com>
2023-12-12 22:50:59 -0500

    finished nav bar !

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -3,48 +3,57 @@ import Link from 'next/link'
 export default function NavBar() {
     return (
     <nav>
-        <h1> Five Star Souvenirs</h1>
-        
-        <a href="/api/auth/login">Login</a>
-        <a href="/api/auth/logout">Logout</a>
-        
         {/* <img className="h-auto max-w-full" src="/app/imgs/Logo.png" alt="image"> */}
         {/* <img className="w-[204px] h-[84px]" src="logo1/Logo.png" /> */}
         
         {/* Nav bar */}
 
-        <nav className="bg-gray-100">
+        <nav className="bg-red-100">
             {/* to indent: max-w-5xl */}
-            <div className="px-8 mx-auto border border-red-400">
-
-                <div class="flex justify-between">
-
-                    <div class="flex space-x-4">
+            <div className="px-4 mx-auto border">
+                <div className="flex justify-between">
+                    <div className="flex space-x-4">
 
                         {/* logo */}
-                        <div class="mr-4">
-                            <a href="#" class="flex items-center py-3 px-2 text-gray-700">
-                                <svg class="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <div className="mr-4">
+                            <a href="#" className="flex items-center py-5 px-3 text-gray-700">
+                                <svg className="h-6 w-6 mr-1 text-red" xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                                 </svg>
-                                <span>Five Star Souvenirs</span>
+                                <span className="font-bold">Five Star Souvenirs</span>
                             </a>
                         </div>
 
                         {/* Primary Nav */}
-                        <div class="flex items-center space-x-3">
-                            <a href="/order/new-york" class="">Order</a>
-                            <a href="/contact" class="">Contact</a>
-                            <a href="/about-us" class="">About Us</a>
+                        <div className="flex items-center space-x-1">
+                            <a href="/" className="py-5 px-3 text-gray-700 hover:text-gray-900">Home</a>
+                            <a href="/order/new-york" className="py-5 px-3 text-gray-700 hover:text-gray-900">Order</a>
+                            <a href="/contact" className="py-5 px-3 text-gray-700 hover:text-gray-900">Contact</a>
+                            <a href="/about-us" className="py-5 px-3 text-gray-700 hover:text-gray-900">About Us</a>
                         </div>
 
                     </div>
 
-                    <div>secondary nav</div>
+                    {/* login/logout */}
+                    <div className="flex items-center space-x-1">
+                        <a href="/api/auth/login" className="py-5 px-3">Login</a>
+                        <a href="/api/auth/logout" className="py-2 px-3 bg-blue-400 hover:bg-blue-300 text-blue-900 hover:text-blue-800 rounded">Logout</a>
+                    </div>
+
+                    {/* mobile button here */}
+                    {/* <div class="md:hidden flex items-center">
+                        mobile thing
+                    </div> */}
 
                 </div>
 
             </div>
+
+            {/* mobile menu */}
+            {/* <div class="">
+                <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">order</a>
+                <a href="#" class="block">ya</a>
+            </div> */}
         </nav>
         
         

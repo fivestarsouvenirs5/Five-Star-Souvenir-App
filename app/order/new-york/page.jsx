51c3e0c9b31896bff459a68a25c5a6df2b79c885
Christina Tashji <christinast2020@gmail.com>
2023-12-13 23:08:05 -0500

    categories are now in boxes and styled

--- app/order/new-york/page.jsx
+++ app/order/new-york/page.jsx
@@ -11,16 +11,18 @@ export default async function OrderNY() {
     const categories = await fetchCategories()
 
     console.log({categories})
-  return (
+
+    return (
     <main>
-        <h2 class="text-red-600">Categories</h2>
-            {categories.map((category) => (
-                <div key={category.category_id} >
-                    <CategoryNY category = {category} />
-                </div>
-            ))}
-        <div>
-            
+        <h2>Categories</h2>
+        <div className = "px-20 py-10 justify-center">
+            <div className="grid grid-cols-3 gap-5">
+                {categories.map((category) => (
+                    <div className="border p-5 rounded-md text-center bg-blue-100 hover:bg-blue-300" key={category.category_id} >                        
+                        <CategoryNY category = {category} />
+                    </div>
+                ))}
+            </div>
         </div>
         
 

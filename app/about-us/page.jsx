61e58d417fbf275cc9c794e4e87b0d6cbb87fc12
Christina Tashji <christinast2020@gmail.com>
2023-12-15 11:06:40 -0500

    SASHAS contact + aboutus done!

---
+++ app/about-us/page.jsx
@@ -0,0 +1,19 @@
+export default function AboutUs() {
+    return (
+        <main >
+            {/* <div>
+                <div className="w-[4000px] h-[1000px] bg-zinc-300" />
+                <div className="w-[10px] h-[400px]"><span className="text-red-500 text-[32px] font-bold font-['Inter']"></span><span 
+                className="text-black text-[40px] font-bold font-['Inter']">Favorite </span><span clsasName="text-red-500 text-[40px] font-bold font-['Inter']"></span><span 
+                className="text-black text-[40px] font-bold font-['Inter']">Stop for affordable </span><span className="text-red-500 text-[40px] font-bold font-['Inter']"></span><span 
+                className="text-black text-[40px] font-bold font-['Inter']">Souvenirs with creative designs and innovative ideas.</span></div>
+            </div> */}
+            {/* we are five star souvenirs (BOX NOT CENTERED) */}
+            <div className="left-[50px] top-[40px] content-center w-[1195px] h-[143px] relative">
+                <div className="w-[1195px] h-[500px] left-0 top-0 absolute border border-red-500 bg-zinc-300"  />
+                <div className="left-[40px] top-[40px] absolute text-black text-[40px]">Favorite Stop for affordable souvenirs with creative designs and innovative ideas</div>
+            </div>
+            
+        </main>
+    )
+}

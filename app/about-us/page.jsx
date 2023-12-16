425530a89e879ffc3143cca15f5f11f5bd50d8d7
riasha24 <riajshah@gmail.com>
2023-12-15 20:09:42 -0500

    changed some styling for about us page

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -9,9 +9,10 @@ export default function AboutUs() {
                 className="text-black text-[40px] font-bold font-['Inter']">Souvenirs with creative designs and innovative ideas.</span></div>
             </div> */}
             {/* we are five star souvenirs (BOX NOT CENTERED) */}
+            <h1 className="text-center py-5 lg:text-4xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(200,0,0,0.8]">About Us</h1>
             <div className="left-[50px] top-[40px] content-center w-[1195px] h-[143px] relative">
                 <div className="w-[1195px] h-[500px] left-0 top-0 absolute border border-red-500 bg-zinc-300"  />
-                <div className="left-[40px] top-[40px] absolute text-black text-[40px]">Favorite Stop for affordable souvenirs with creative designs and innovative ideas</div>
+                <div className="left-[40px] top-[40px] absolute text-black text-[40px] lg:text-2xl">Favorite Stop for affordable souvenirs with creative designs and innovative ideas</div>
             </div>
             
         </main>

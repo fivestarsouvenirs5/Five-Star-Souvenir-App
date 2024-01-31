a28f56c1e243611eee59904dec5b53a514d31de7
anastasiak36 <akelnik.9@gmail.com>
2024-01-31 10:12:29 -0500

    about us text

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -12,7 +12,27 @@ export default function AboutUs() {
             <h1 className="text-center py-5 lg:text-4xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(200,0,0,0.8]">About Us</h1>
             <div className="left-[50px] top-[40px] content-center w-[1195px] h-[143px] relative">
                 <div className="w-[1195px] h-[500px] left-0 top-0 absolute border border-red-500 bg-zinc-300"  />
-                <div className="left-[40px] top-[40px] absolute text-black text-[40px] lg:text-2xl">Favorite Stop for affordable souvenirs with creative designs and innovative ideas</div>
+                <div className="left-[40px] top-[40px] absolute text-black text-[40px] lg:text-2xl">
+                    {/* need to make letters red */}
+                    <h3>Favorite Stop for affordable Souvenirs with creative designs and innovative ideas.</h3>
+                    <p>Five Star Souvenirs Inc. is a family-owned wholesale corporation that has been around for over 20 years.  It was started as a small private company specializing in assembling and distributing frame-around magnets with images of New York City. They were assembled from parts exclusively made in the USA in the garage. Soon, the magnets became very popular among tourists and the company had to extend in order to keep up with the demand.  That was the starting point for future growth and success. </p> 
+                    <br></br>
+                    <p> By using innovative ideas, Five Star Souvenirs has developed many new designs and incorporated them into different items creating various souvenirs of New York City. Our variety includes magnets, mugs, stationary, kitchen, postcards, keychains, bags, purses and much more. </p> 
+                    <br></br>
+                    <p>The company has been growing and moved to a bigger warehouse located in NJ in 2014. At the same time, the company started to work with the overseas vendors, hired more salespeople and widened its customer base to other areas. The company continued working on generating new ideas and looking for new opportunities.</p>
+                    <br></br>
+                    <p>Today, we are working with more than 20 suppliers, and have a team of dedicated salespeople who market and distribute about 50 different types of souvenirs to more than 100 retail stores. We consider all of them to be our partners, who help us reach our main goal: to produce affordable high-quality souvenirs. We are really thankful to all of them for making this possible.
+                    </p>
+                    <br></br>
+                    <h3>What sets Five Star Souvenirs Inc. from other companies is:</h3>
+                    <ul>
+                        <li>ability to create unique designs by using real images and drawings of New York City and its landmarks as well as other tourist destinations. </li>
+                        <li>working with our own photographer and designer to create the most unique and appealing products.</li>
+                        <li>dedication to always make high quality items using eco-friendly materials when possible.</li>
+                        <li>aspiration to make our retail partners happy by setting good wholesale prices and working individually with each of them.</li>
+                        <li>ability to make souvenirs that are colorful, creative and useful at the same time.</li>
+                    </ul>
+                </div>
             </div>
             
         </main>

9607051c675a156d864eb3c294f6ffdad267c08d
anastasiak36 <akelnik.9@gmail.com>
2024-02-02 09:46:37 -0500

    changed next.config file to include externals

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // Your existing Next.js configuration options here
+    // For example:
+    // reactStrictMode: true,
+    // webpack5: true,
+  
+    webpack: (config, { isServer }) => {
+      // Add external modules for server build only
+      if (isServer) {
+        config.externals.push({
+            'node:buffer': 'buffer',
+            'node:fs': 'fs',
+            'node:https': 'https',
+            'node:http': 'http',
+            'node:net': 'net'
+        });
+      }
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;

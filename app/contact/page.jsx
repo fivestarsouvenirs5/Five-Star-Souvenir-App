99de1b262dd23d3bd71825d493e028586c4e91ef
Christina Tashji <christinast2020@gmail.com>
2024-02-06 09:20:59 -0500

    commented out axios

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,5 @@
 // import React, { useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 export default function Contact() {
 //     const [fullname, setFullname] = useState("");

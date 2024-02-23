561b02977c7a07a866d03bdcc19b0ca35032913d
Sasha Gupta <sashagupta75@gmail.com>
2024-02-23 02:46:57 -0500

    all cart customizations

--- app/components/cart.jsx
+++ app/components/cart.jsx
@@ -18,11 +18,11 @@ function CartEntry({ entry, removeItem }) {
         <p>
            {entry.quantity} x {' '} 
            {entry.product_data.size}
-           &quot;{entry.name}&quot; ={' '}
+           &quot;{entry.name}&quot; ~ {' '}
           {entry.formattedValue}
           
         </p>
-        <button onClick={() => removeItem(entry.id)}>Remove</button>
+        <button onClick={() => removeItem(entry.id)} className="bg-red-400 text-white px-2 py-0 rounded-m mb-4">Remove</button>
       </div>
     )
   }
@@ -55,37 +55,37 @@ export default function Cart() {
           
         
         return(
-          <div className="lg:w-96 md:w-8/12 w-full bg-red-100 float-right h-full">
+          <div className="lg:w-96 md:w-8/12 w-full bg-red-100 float-right h-full mb-20  top-20 right-10">
           <div className="flex flex-col lg:h-screen h-auto lg:px-8 md:px-7 px-4 lg:py-20 md:py-10 py-6 justify-between overflow-y-auto rounded-md">
-          <div>
-              <p className="lg:text-4xl text-3xl font-bold leading-9 text-gray-800 dark:text-black">Summary</p>
-              <p className="lg:text-1xl text-1xl font-black leading-9 text-gray-800 dark:text-black">Items in Cart:</p>
-          </div>
-          {cartEntries.length === 0 ? <p>Cart is empty.</p> : null}
-          {cartEntries.length > 0 ? (
+            <div>
+              <p className="lg:text-5xl text-4xl font-bold leading-10 text-gray-800 dark:text-black mb-2">Summary</p>
+              <p className="lg:text-2xl text-xl font-black leading-8 text-gray-800 dark:text-black mb-4">Items in Cart:</p>
+            </div>
+            {cartEntries.length === 0 ? <p>Cart is empty.</p> : null}
+            {cartEntries.length > 0 ? (
               <>
-              <button onClick={() => clearCart()}>Clear cart</button>
-              {cartEntries}
+              <button onClick={() => clearCart()} className="bg-red-500 text-white px-3 py-1 rounded-md mb-4">
+                Clear cart
+              </button>             
+                 {cartEntries}
               </>
-          ) : null}
-          <div>
-              <div className="flex items-center justify-between pt-16">
-              </div>
+            ) : null}
+            <div>
+              <div className="flex items-center justify-between pt-16"></div>
               {/* <TotalPrice total={formattedTotalPrice}/> */}
               <div className="flex items-center pb-6 justify-between lg:pt-5 pt-20">
-              <p className="text-2xl leading-normal text-gray-800 dark:text-black">Total</p>
-              <h3 className="text-2xl font-bold leading-normal text-right text-gray-800 dark:text-black" >{formattedTotalPrice}</h3>
-            </div>
-              <div className = "flex items-center justify-center text-base leading-none w-full py-5 bg-blue-300 border-4 border-black border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white hover:bg-blue-100">
-                  <svg className="w-6 h-6 text-gray-800 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
-                  </svg>
-                  <OrderButton />
+                <p className="text-3xl leading-normal text-gray-800 dark:text-black">Total: </p>
+                <h3 className="text-3xl font-bold leading-normal text-right text-gray-800 dark:text-black">{formattedTotalPrice}</h3>
               </div>
-              
+              <div className="flex items-center justify-center text-lg leading-none w-full py-5 bg-blue-300 border-4 border-black border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white hover:bg-blue-100">
+                <svg className="w-8 h-8 text-gray-800 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
+                </svg>
+                <OrderButton />
+              </div>
+            </div>
           </div>
-          </div>
-          </div>
+        </div>
       )
     }
     else {

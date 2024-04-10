e3167d094242596bf3edfd47abe3f8499ce120d2
anastasiak36 <akelnik.9@gmail.com>
2024-04-10 16:29:11 -0400

    added email send after new user created

--- app/api/signup/route.js
+++ app/api/signup/route.js
@@ -1,6 +1,6 @@
 export async function POST(request) {
+  const newUserDetails = await request.json()
   try {
-    const newUserDetails = await request.json()
     console.log(newUserDetails)
     console.log(newUserDetails.admin_approval)
     const fetchURL = 'https://' + process.env.AUTH0_DOMAIN + '/dbconnections/signup'
@@ -20,13 +20,36 @@ export async function POST(request) {
         user_metadata: { phonenumber: newUserDetails.phone_number, adminapproval: newUserDetails.admin_approval },
       })
       })
+      if (response.ok) {
+        const brevo = require('@getbrevo/brevo');
+        let apiInstance = new brevo.TransactionalEmailsApi();
+        let apiKey = apiInstance.authentications['apiKey'];
+        apiKey.apiKey = process.env.BREVO_API_KEY;
+        
+        let sendSmtpEmail = new brevo.SendSmtpEmail(); 
+        sendSmtpEmail.subject = "New user request";
+        sendSmtpEmail.htmlContent = `<html><body>
+                                        <h2>Name: ${newUserDetails.first_name} ${newUserDetails.last_name}</h2>
+                                        <h2>Email: ${newUserDetails.user_email}</h2>
+                                        <h2>Phone: ${newUserDetails.phone_number}</h2>
+                                        <h2>Store Name: name</h2>
+                                        <h2>Store Address: *street* *city* *state* *zip*</h2>
+                                    </body></html>`;
+        sendSmtpEmail.sender = { name: 'App', email: 'akelnik.9@gmail.com' };
+        sendSmtpEmail.to = [{ email: 'akelnik.9@gmail.com', name: 'Five Star Souvenirs' }];
+      
+        
+        const headers = { 'Content-Type': 'application/json' };
+      
+        const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
+      }
       return response;
+     
   }
   catch (error) {
     console.log("signup error", error)
+    return response;
   }
-  console.log(response);
-  console.log(response.data);
-   
+
       
 }
